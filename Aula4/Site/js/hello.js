79b7e3c6055dadac779b5a9ce7fd5db2db6a1539
Guto Xavier <gutoffline@gmail.com>
2017-12-21 22:11:17 -0200

    Aula4

---
+++ Aula4/Site/js/hello.js
@@ -0,0 +1,38 @@
+console.log("Olá, Mundo");
+
+/* ADIÇÃO */
+console.log("12 + 13");
+console.log(12+13);
+
+/* MULTIPLICAÇÃO */
+console.log("14 + 3");
+console.log(14 + 3);
+
+/* SUBTRAÇÃO */
+console.log("10 - 4");
+console.log(10 - 4);
+
+/* DIVISÃO */
+console.log("25 / 5");
+console.log(25 / 5);
+
+/* MÓDULO */
+console.log("23 % 2");
+console.log(23 % 2);
+
+
+var curso = "Desenvolvimento Front-End";
+console.log(curso);
+
+
+
+
+
+
+
+
+
+
+
+
+
